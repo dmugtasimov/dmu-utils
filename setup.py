35092ed1de97cb7952de49ed72b095a0ab2ade8f
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2017-03-24 23:35:00 +0300

    Python packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+version = '0.0.1'
+
+setup(
+    name='dmu-utils',
+    version=version,
+    description='DMU Utils',
+    packages=['dmu_utils'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[],
+    extras_require={},
+    include_package_data=True,
+)

6c8561fc5fc75262b9f083e524c11d2cca312882
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2017-05-16 16:51:23 +0300

    Schematics to SQLAlchemy convertion support + SQLAlchemy utils enchancements

--- TODO.rst
+++ TODO.rst
@@ -1,3 +1,4 @@
 * [HIGH] Produce documentation
 * [HIGH] Support Python 3
 * [HIGH] Restructure rst-files
+* [HIGH] Improve unittest code coverage
--- dmu_utils/schematics/models.py
+++ dmu_utils/schematics/models.py
@@ -5,6 +5,21 @@ import json
 from schematics import Model
 
 
+def get_schematics_model_fields(model, include=None, exclude=None):
+    exclude = exclude or ()
+
+    model_fields = model._fields
+    if include is None:
+        result = model_fields.copy()
+        for field_name in exclude:
+            result.pop(field_name, None)
+    else:
+        result = {field_name: model_fields[field_name] for field_name
+                  in include if field_name not in exclude}
+
+    return result
+
+
 class CustomModel(Model):
 
     def __init__(self, *args, **kwargs):
